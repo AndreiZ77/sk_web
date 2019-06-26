f3df6b06b6f20aeae0e7fe3784d48e9b41e08eac
AndreiZ77 <hi_andy@mail.ru>
2019-06-26 17:33:09 +0400

    wsgi app file

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ sudo /etc/init.d/nginx restart
 
 # gunicorn
 #sudo ln -s /home/box/web/etc/gunicorn.py /etc/gunicorn.d/gunicorn.py
-cd /home/box/web && sudo gunicorn -b 0.0.0.0:8080 gunicorn hello:app
+cd /home/box/web && sudo gunicorn -b 0.0.0.0:8080 hello:app &
 #cd /home/box/web && sudo gunicorn -c /home/box/web/etc/gunicorn.py hello:app
 
 # settings for mysql
